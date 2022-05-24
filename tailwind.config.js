28c0a92e0fe066cdbe64188516d5ffcb98565aa4
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-24 18:18:13 +0600

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#EF185A",
+          secondary: "#20AFB3",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
