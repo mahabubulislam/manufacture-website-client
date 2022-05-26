f91099f6fe667cb177358f0238c2243c8389a9b6
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-27 03:59:21 +0600

    reviews load from server

--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -5,8 +5,14 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import Review from './Review';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 
 const Reviews = () => {
+    const { data: reviews,isLoading, refetch } = useQuery('reviews', () => fetch('http://localhost:5000/reviews').then(res => res.json()))
+    if(isLoading){
+        return <Loading/>
+    }
     return (
         <div>
             <h3 className='text-3xl font-bold text-center '>Customers Reviews</h3>
@@ -24,15 +30,12 @@ const Reviews = () => {
                 modules={[Autoplay, Pagination, Navigation]}
                 className="mySwiper"
             >
-                <SwiperSlide><Review/></SwiperSlide>
-                <SwiperSlide><Review/></SwiperSlide>
-                <SwiperSlide><Review/></SwiperSlide>
-                <SwiperSlide><Review/></SwiperSlide>
-                <SwiperSlide><Review/></SwiperSlide>
-                <SwiperSlide><Review/></SwiperSlide>
-                <SwiperSlide><Review/></SwiperSlide>
-                <SwiperSlide><Review/></SwiperSlide>
-                <SwiperSlide><Review/></SwiperSlide>
+                {
+                    reviews.map(review => <SwiperSlide>
+                        <Review key={review._id} reviews={review} refetch={refetch} />
+                    </SwiperSlide>)
+                }
+
             </Swiper>
         </div>
     );
