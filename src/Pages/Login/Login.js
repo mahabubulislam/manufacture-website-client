95d5f87456ec6a09c94910cbd9c153652b0aea47
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-26 12:40:38 +0600

    social login and email pass login added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 import SocialLogin from './SocialLogin';
 
 const Login = () => {
-    const handleLogin = e => {
-
-    }
+    const { register, handleSubmit } = useForm();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const onSubmit = data => {
+        signInWithEmailAndPassword(data?.email, data?.password)
+    };
     return (
 
         <div className="card flex-shrink-0 w-full mx-auto  max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
-                <form onSubmit={handleLogin}>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div >
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
+                        <input type="email" placeholder="Email" {...register("email")} className="input input-bordered" required />
                     </div>
                     <div >
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <input type="password" placeholder="Password" {...register("password")} className="input input-bordered" required />
                         <label className="label">
                             <p className="label-text-alt cursor-pointer text-blue-600">Forgot password?</p>
                         </label>
@@ -33,7 +42,7 @@ const Login = () => {
                         <input type="submit" className="btn btn-primary block mx-auto" value="Login" />
                     </div>
                 </form>
-                <SocialLogin/>
+                <SocialLogin />
             </div>
         </div>
     );
--- src/Pages/Login/SocialLogin.js
+++ src/Pages/Login/SocialLogin.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {FaFacebook,FaGoogle} from 'react-icons/fa';
+import { useSignInWithGoogle, useSignInWithFacebook } from 'react-firebase-hooks/auth';
+import { FaFacebook, FaGoogle } from 'react-icons/fa';
+import auth from '../../firebase.init';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithFacebook, fbUser, fbLoading, fbError] = useSignInWithFacebook(auth);
     return (
         <div>
             <div className="divider">OR</div>
-            <button className="btn btn-outline flex justify-evenly items-center  hover:bg-accent w-full my-2 mx-auto">Continue with Google <FaGoogle></FaGoogle> </button>
-            <button className="btn btn-outline flex justify-evenly items-center  hover:bg-accent w-full mt-2 mx-auto">Continue with Facebook <FaFacebook></FaFacebook></button>
+            <button onClick={()=>signInWithGoogle()} className="btn btn-outline flex justify-evenly items-center  hover:bg-accent w-full my-2 mx-auto">Continue with Google <FaGoogle></FaGoogle> </button>
+            <button onClick={()=>signInWithFacebook()} className="btn btn-outline flex justify-evenly items-center  hover:bg-accent w-full mt-2 mx-auto">Continue with Facebook <FaFacebook></FaFacebook></button>
         </div>
     );
 };
