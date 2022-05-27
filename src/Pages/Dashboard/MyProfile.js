e7bdd11458e88e98b03402bdc926b18c6272750c
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-27 21:09:17 +0600

    my profile info displayed in ui

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
+import { useQuery } from 'react-query';
 import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 import Loading from '../Shared/Loading';
 
 const MyProfile = () => {
     const [user, loading] = useAuthState(auth);
-    const { register, handleSubmit , reset} = useForm();
-    if (loading) {
+    const { register, handleSubmit, reset , defaultValue} = useForm();
+    const { data, isLoading } = useQuery('my-profile', () => fetch(`http://localhost:5000/my-profile/${user?.email}`).then(res=>res.json()));
+    if (loading || isLoading) {
         return <Loading />
     }
+    
     const email = user?.email;
     const name = user?.displayName;
-
     const onSubmit = data => {
         const user = {
             email,
@@ -30,24 +32,25 @@ const MyProfile = () => {
             village: data?.village,
             year: data?.year
         }
-        fetch(`http://localhost:5000/my-profile/${email}`,{
-            method:'PUT',
-            headers:{
-                'content-type':'application/json'
+        fetch(`http://localhost:5000/my-profile/${email}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
             },
             body: JSON.stringify(user)
         })
-        .then(res=>res.json())
-        .then(data=>{
-            if(data.acknowledged===true){
-                toast.success('Your Profile updated');
-                reset()
-            }
-            else{
-                toast.error('Something Went Wrong, Please try again later')
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged === true) {
+                    toast.success('Your Profile updated');
+                    reset()
+                }
+                else {
+                    toast.error('Something Went Wrong, Please try again later')
+                }
+            })
     };
+    const { board, district, division, exam, institute, linkdin, phone, policeStation, result, subject, village, year, } = data
 
     return (
         <div>
@@ -65,7 +68,7 @@ const MyProfile = () => {
                             </div>
                         </div>
                     </div>
-
+                    
                     <div className="divider"></div>
 
                     <div>
@@ -78,27 +81,27 @@ const MyProfile = () => {
                                     <label class="label">
                                         <span class="label-text font-semibold">Exam/Degree Name</span>
                                     </label>
-                                    <input type="text" {...register("exam", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input  type="text" {...register("exam", { required: true })} defaultValue={exam} class="input input-bordered w-full max-w-xs" placeholder='Exam name'/>
                                     <label class="label">
                                         <span class="label-text font-semibold">Board</span>
                                     </label>
-                                    <input type="text" {...register("board", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input type="text" {...register("board", { required: true })} defaultValue={board} class="input input-bordered w-full max-w-xs" placeholder='Board Name'/>
                                     <label class="label">
                                         <span class="label-text font-semibold">Subject/Group</span>
                                     </label>
-                                    <input type="text" {...register("subject", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input type="text" {...register("subject", { required: true })} defaultValue={subject} class="input input-bordered w-full max-w-xs" placeholder='Group/ Subject Name'/>
                                     <label class="label">
                                         <span class="label-text font-semibold">Insititute</span>
                                     </label>
-                                    <input type="text" {...register("institute", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input type="text" {...register("institute", { required: true })} defaultValue={institute} class="input input-bordered w-full max-w-xs" placeholder='Institute name'/>
                                     <label class="label">
                                         <span class="label-text font-semibold">Year</span>
                                     </label>
-                                    <input type="text" {...register("year", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input type="text" {...register("year", { required: true })} defaultValue={year} class="input input-bordered w-full max-w-xs" placeholder='Passing Year'/>
                                     <label class="label">
                                         <span class="label-text font-semibold">Result</span>
                                     </label>
-                                    <input type="text" {...register("result", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input type="text" {...register("result", { required: true })} defaultValue={result} class="input input-bordered w-full max-w-xs" placeholder='GPA / CGPA'/>
                                 </div>
 
 
@@ -107,27 +110,27 @@ const MyProfile = () => {
                                     <label class="label">
                                         <span class="label-text font-semibold">Division</span>
                                     </label>
-                                    <input type="text" {...register("division", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input type="text" {...register("division", { required: true })} defaultValue={division} class="input input-bordered w-full max-w-xs" placeholder='Your Division'/>
                                     <label class="label">
                                         <span class="label-text font-semibold">District</span>
                                     </label>
-                                    <input type="text" {...register("district", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input type="text" {...register("district", { required: true })} defaultValue={district} class="input input-bordered w-full max-w-xs" placeholder='Your District'/>
                                     <label class="label">
                                         <span class="label-text font-semibold">Police Station</span>
                                     </label>
-                                    <input type="text" {...register("policeStation", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input type="text" {...register("policeStation", { required: true })} defaultValue={policeStation} class="input input-bordered w-full max-w-xs" placeholder='Your Police Station'/>
                                     <label class="label">
                                         <span class="label-text font-semibold">Village</span>
                                     </label>
-                                    <input type="text" {...register("village", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input type="text" {...register("village", { required: true })} defaultValue={village} class="input input-bordered w-full max-w-xs" placeholder='Your Village'/>
                                     <label class="label">
                                         <span class="label-text font-semibold">Phone</span>
                                     </label>
-                                    <input type="text" {...register("phone", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input type="text" {...register("phone", { required: true })} defaultValue={phone} class="input input-bordered w-full max-w-xs" placeholder='Your Phone Number'/>
                                     <label class="label">
-                                        <span class="label-text font-semibold">Linkdin</span>
+                                        <span class="label-text font-semibold">Linkedin</span>
                                     </label>
-                                    <input type="text" {...register("linkdin", { required: true })} placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                                    <input type="text" {...register("linkdin", { required: true })} defaultValue={linkdin} class="input input-bordered w-full max-w-xs" placeholder='Your Linkedin Profle Link'/>
                                 </div>
                             </div>
                             <input className='btn btn-primary block mx-auto' type="submit" value="Update Profile" />
